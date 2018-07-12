908ab7db4df681217f78e13d7023dc46d1a6ea72
Dana Palmer <roiai4@hotmail.com>
2018-07-12 05:03:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,11 @@ def oxford_comma(array)
 if array.size < 2 
   array.join
   elsif array.size == 2 
-array.join(" and ")
-elsif array.size == 3
-array[0..1].join(", ") + " and " + array.last
+    array.join(" and ")
+  elsif array.size == 3
+    array[0..1].join(", ") + ", and " + array.last
 else
-  array > 3 
-  array[0..3].join(", ") + " and " + array.last
+  array.size > 3 
+  array[0..array.length-2].join(", ")  + ", and " + array.last
 end
 end
