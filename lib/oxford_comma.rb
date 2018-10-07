1ca9d103c08c3006a76b04a54d27ad605dfe3d42
Dana Palmer <roiai4@hotmail.com>
2018-10-07 14:57:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   else array.size > 2
-    array.last.insert(0, "and ")
+    array[0..array.length-2].join(", ")  + ", and " + array.last
   end
 end
