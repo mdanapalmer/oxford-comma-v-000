012d111789350a357c3cc8fc1ace7ecf9eaf9930
Dana Palmer <roiai4@hotmail.com>
2018-10-07 14:53:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
   if array.size < 2
     array.join
-  else array.size == 2
+  elsif array.size == 2
     array.join(" and ")
+  else array.size > 2
+    array.join(", ")
   end
 
 end
