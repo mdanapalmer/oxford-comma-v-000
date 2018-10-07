1178539081826b865a17fb332ab8ccba9c167fdb
Dana Palmer <roiai4@hotmail.com>
2018-10-07 14:58:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,10 @@
 def oxford_comma(array)
-if array.size < 2 
-  array.join
-  elsif array.size == 2 
+  if array.size < 2
+    array.join
+  elsif array.size == 2
     array.join(" and ")
-  elsif array.size == 3
-    array[0..1].join(", ") + ", and " + array.last
-else
-  array.size > 3 
-  array[0..array.length-2].join(", ")  + ", and " + array.last
-end
+  else array.size > 2
+    array[0..array.size-2].join(", ")  + ", and " + array.last
+    #review this last line
+  end
 end
